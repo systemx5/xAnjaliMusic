e6b25374d8569a2f7aef46f544fc390492562c5a
systemx5 <154720925+systemx5@users.noreply.github.com>
2023-12-29 11:53:03 +0530

    Update __main__.py

--- AnonX/__main__.py
+++ AnonX/__main__.py
@@ -52,12 +52,12 @@ async def init():
     await userbot.start()
     await Anon.start()
     try:
-        await Anon.stream_decall("https://telegra.ph/file/de3464aa7d6bfafdd2dc3.mp4")
+        await Anon.stream_decall("https://te.legra.ph/file/fb71d000872e4e657453f.mp4")
     except:
         pass
     try:
         await Anon.stream_call(
-            "https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4"
+            "https://te.legra.ph/file/a097a5ba79a8982845e37.mp4"
         )
     except NoActiveGroupCall:
         LOGGER("AnonX").error(
